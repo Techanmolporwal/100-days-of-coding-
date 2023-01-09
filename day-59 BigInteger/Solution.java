94f96cf2a9310a9905237dd48b6c25f3483578e8
Techanmolporwal <89104678+Techanmolporwal@users.noreply.github.com>
2023-01-10 00:29:49 +0530

    Create Solution.java

---
+++ day-59 BigInteger/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+ import java.util.*; 
+ import java.math.BigInteger;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+   BigInteger a = scan.nextBigInteger();
+   BigInteger b = scan.nextBigInteger();
+
+    System.out.println(a.add(b));
+    System.out.println(a.multiply(b));
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+}
+}
