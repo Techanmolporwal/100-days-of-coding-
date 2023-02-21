58d4b99eebcca7c1a5690562f6e114df2dceab08
Techanmolporwal <89104678+Techanmolporwal@users.noreply.github.com>
2023-02-22 00:43:08 +0530

    Create GFG.java

---
+++ day-80 bitonic point/GFG.java
@@ -0,0 +1,12 @@
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        // code here
+        int max=arr[0];
+        for(int i =1;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return(max);
+        }
+}
